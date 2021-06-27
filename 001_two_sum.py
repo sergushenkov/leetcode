72a31087f8b33ce7333e5100a4f29faeddaf32e1
sergushenkov <sergushenkov@gmail.com>
2021-06-27 22:46:33 +0300

    001 two sum - add brute force

--- 001_two_sum.py
+++ 001_two_sum.py
@@ -10,8 +10,7 @@ class Solution:
         for i, a in enumerate(nums):
             if (target - a) in res:
                 return [res[target - a], i]
-            else:
-                res[a] = i
+            res[a] = i
 
     # 60 ms	16.2 MB
     # 2*O(n) - create dict and find pair in dict
@@ -49,6 +48,15 @@ class Solution:
                 return [res[i][1], res[j][1]]
         print('not solution')
 
+    # brute force O(n^2)
+    # 3776 ms	14.7 MB
+    @staticmethod
+    def two_sum_bf(nums: List[int], target: int) -> List[int]:
+        for i, num in enumerate(nums):
+            for j, other_num in enumerate(nums[i + 1:]):
+                if num + other_num == target:
+                    return [i, j + i + 1]
+
 
 assert Solution.two_sum(nums=[2, 7, 11, 15], target=9) == [0, 1]
 assert Solution.two_sum(nums=[3, 2, 4], target=6) == [1, 2]
@@ -64,3 +72,8 @@ assert Solution.two_sum_2p(nums=[2, 7, 11, 15], target=9) == [0, 1]
 assert Solution.two_sum_2p(nums=[3, 2, 4], target=6) == [1, 2]
 assert Solution.two_sum_2p(nums=[3, 3], target=6) == [0, 1]
 assert Solution.two_sum_2p(nums=[0, 4, 3, 0], target=0) == [0, 3]
+
+assert Solution.two_sum_bf(nums=[2, 7, 11, 15], target=9) == [0, 1]
+assert Solution.two_sum_bf(nums=[3, 2, 4], target=6) == [1, 2]
+assert Solution.two_sum_bf(nums=[3, 3], target=6) == [0, 1]
+assert Solution.two_sum_bf(nums=[0, 4, 3, 0], target=0) == [0, 3]
